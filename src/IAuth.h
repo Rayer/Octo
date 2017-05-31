049a4b29aa39016a28dea5951e197a4d3861e712
Killercat <rayershih@gmail.com>
2017-06-01 01:14:43 +0800

    Fix several linker issue related to twitcurl

---
+++ src/IAuth.h
@@ -0,0 +1,8 @@
+//
+// Created by Rayer Tung on 31/05/2017.
+//
+
+#ifndef OCTO_IAUTH_H
+#define OCTO_IAUTH_H
+
+#endif //OCTO_IAUTH_H
---
+++ src/Twitter/TwitterOAuth.h
@@ -0,0 +1,16 @@
+//
+// Created by Rayer Tung on 31/05/2017.
+//
+
+#ifndef OCTO_TWITTEROAUTH_H
+#define OCTO_TWITTEROAUTH_H
+
+
+
+class TwitterOAuth {
+
+};
+
+
+
+#endif //OCTO_TWITTEROAUTH_H
